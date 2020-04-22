7690c6460e9c917601e699d01567e096b9030d3d
BKM <bmaibach@gmail.com>
2020-04-22 14:25:42 -0700

    Add HTTP methods to hello view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,10 +1,17 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
+
+
 
 class HelloAPIView(APIView):
     """My first APIView"""
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
+
         """Returns a list of APIView features"""
         an_apiview = [
             'Uses HTTP functions as methods (get, put, post, patch, delete)',
@@ -13,3 +20,26 @@ class HelloAPIView(APIView):
             'Is mapped manually to URLs'
         ]
         return Response({'message': 'Hello', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handle partially updating an object"""
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Handle deleting an object"""
+        return Response({'method': 'DELETE'})
